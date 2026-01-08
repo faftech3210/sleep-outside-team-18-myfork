8f94fa532f72a8a0ad4e20a2b38d78e7fcf25976
Seruhio30 <sh0041314@gmail.com>
2026-01-07 18:54:19 -0600

    individual task 1 sh

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,22 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Leer lo que ya hay en localStorage
+  let cart = JSON.parse(localStorage.getItem("so-cart"));
+  // Si no existe o no es array, inicializarlo
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+  // Agregar el nuevo producto
+  cart.push(product);
+
+  // Guardar el array completo
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
